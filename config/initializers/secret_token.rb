c717002c9cdf97cd7df37f78d1172e67d6de3be1
Ariun <ariunik@mail.ru>
2012-01-18 18:38:25 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RiaDemo::Application.config.secret_token = '8714d9d27685b9470906ceea599269815082e12349e9724fb25cd1390d49c8e14fdfced1888daacfd19b870a75de3fb1603f838750fb91c6878588e805043fea'
