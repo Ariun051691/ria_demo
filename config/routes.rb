ae6415e379dbfeda000de09007b1efbe64f10f45
Ariun <ariunik@mail.ru>
2012-01-29 05:14:00 +0400

    Finished first cut of the User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 RiaDemo::Application.routes.draw do
-  get "users/new"
-match '/signup', :to => 'users#new'
+resources :users
+  match '/signup',  :to => 'users#new'
 match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
+  
    root :to => 'pages#home'
 
   # The priority is based upon order of creation:
