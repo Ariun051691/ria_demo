6325c7219b232a023522facee4726cd13c720c35
Ariun <ariunik@mail.ru>
2012-02-02 16:58:44 +0400

    User signup complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,11 +28,16 @@ validates :password,   :presence     => true,
   def has_password? (submitted_password)
 	encrypted_password == encrypt(submitted_password)
   end
-  def self.authenticate(email, submitted_password)
+  
+def self.authenticate(email, submitted_password)
     user = find_by_email(email)
-    return nil  if user.nil?
-    return user if user.has_password?(submitted_password)
+    user && user.has_password?(submitted_password) ? user : nil
   end
+  #def self.authenticate(email, submitted_password)
+   # user = find_by_email(email)
+    #return nil  if user.nil?
+    #return user if user.has_password?(submitted_password)
+  #end
 
   private
 
